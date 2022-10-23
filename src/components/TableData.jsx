39688e43846ca52bf7967278b35c4fe2a09c50f6
Pedro Henrique Marques <opedrohenriquemarques@gmail.com>
2022-10-23 16:19:38 -0300

    test without scrollbar

--- src/components/TableData.jsx
+++ src/components/TableData.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import TableScrollbar from "react-table-scrollbar";
 
 export default function TableData({ parsedData, columns, values }) {
   return (
@@ -7,43 +6,41 @@ export default function TableData({ parsedData, columns, values }) {
       {parsedData.length > 0 && (
         <div className="flex flex-col bg-zinc-100 rounded-sm ml-6">
           <div className="overflow-hidden rounded-sm">
-            <TableScrollbar rows={25}>
-              <table className="border-collapse text-sm font-sans shadow-md">
-                <thead className="border-b">
-                  <tr>
-                    {columns.map((rows, index) => {
-                      return (
-                        <th
-                          scope="col"
-                          className="text-sm font-medium text-gray-900 px-6 py-4 text-left bg-zinc-100"
-                          key={index}
-                        >
-                          {rows}
-                        </th>
-                      );
-                    })}
-                  </tr>
-                </thead>
-                <tbody>
-                  {values.map((value, index) => {
+            <table className="border-collapse text-sm font-sans shadow-md">
+              <thead className="border-b">
+                <tr>
+                  {columns.map((rows, index) => {
                     return (
-                      <tr key={index}>
-                        {value.map((val, i) => {
-                          return (
-                            <td
-                              className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                              key={i}
-                            >
-                              {val}
-                            </td>
-                          );
-                        })}
-                      </tr>
+                      <th
+                        scope="col"
+                        className="text-sm font-medium text-gray-900 px-6 py-4 text-left bg-zinc-100"
+                        key={index}
+                      >
+                        {rows}
+                      </th>
                     );
                   })}
-                </tbody>
-              </table>
-            </TableScrollbar>
+                </tr>
+              </thead>
+              <tbody>
+                {values.map((value, index) => {
+                  return (
+                    <tr key={index}>
+                      {value.map((val, i) => {
+                        return (
+                          <td
+                            className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                            key={i}
+                          >
+                            {val}
+                          </td>
+                        );
+                      })}
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
           </div>
         </div>
       )}
